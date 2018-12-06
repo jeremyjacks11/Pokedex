2aa8af8967e3c931ed8f2908f86aff05759e92a4
Jeremy Jacks <jeremyjacks@jeremys-macbook-pro.local>
2018-12-05 16:56:53 -0800

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Jeremy Jacks on 12/5/18.
+//  Copyright © 2018 Jeremy Jacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
